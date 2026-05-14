2c42847309c24fe20a39393e09f31dc992a05630
rital <ritalp0128@gmail.com>
2026-05-14 12:34:33 +0530

    Initaial Commit

---
+++ simple-cicd-app/frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Simple CI/CD App</h1>
+        <p>Deployed via GitHub Actions to AWS EC2</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
